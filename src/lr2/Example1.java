65d2a2e937b83b12fcc92ba13a298e29696c5fd2
AdamAushev <adam.aushev.913@gmail.com>
2022-11-24 15:05:21 +0300

    add lr1

---
+++ src/lr2/Example1.java
@@ -0,0 +1,7 @@
+package lr2;
+
+public class Example1 {
+        public static void main(String[] args) {
+            System.out.println("Привет из Eclipse");
+        }
+    }
---
+++ src/lr2/Example4.java
@@ -0,0 +1,17 @@
+package lr2;
+import java.util.Scanner;
+public class Example4 {
+    public static void main(String[] args) {
+             System.out.println("ВВедите число:");
+        try (Scanner input = new Scanner(System.in)) {
+            int x;
+            x = input.nextInt();
+            if (x % 4 == 0 &x>10){
+                System.out.println("Делится на 4");
+            } else {
+                System.out.println("Не делится на 4 или меньше 10");}
+        }
+
+    }
+
+}
