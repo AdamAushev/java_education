24d3c5b0dbcaf0707f847a727300cd96ae7136e6
AdamAushev <adam.aushev.913@gmail.com>
2022-12-16 17:39:03 +0300

    add lr4

---
+++ src/lr4/Example2.java
@@ -0,0 +1,15 @@
+package lr4;
+import java.util.Scanner;
+public class Example2 {
+    public static void main(String[] args) {
+                int height, width, i, j;
+                Scanner sc = new Scanner(System.in);
+                height = 11;
+                width = 23;
+                System.out.println(" " + height + "-" + width + "");
+                for (i = 0; i < height; ++i, System.out.println())
+                    for (j = 0; j < width; ++j)
+                        System.out.print("*");
+            }
+        }
+
